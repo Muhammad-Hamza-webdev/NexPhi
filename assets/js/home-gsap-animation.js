c533cbbb6e078e17f9b16ba7bd667e7513757d32
Muhammad Hamza <muhammadhamza.webdev@gmail.com>
2025-04-28 23:34:17 +0530

    about section

--- assets/js/home-gsap-animation.js
+++ assets/js/home-gsap-animation.js
@@ -1,22 +1,4 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const title = document.querySelector(".hero-section h1");
-//   const chars = title.textContent.split("");
-//   title.innerHTML = chars.map((char) => `<span>${char}</span>`).join("");
-
-//   gsap.fromTo(
-//     title.querySelectorAll("span"),
-//     { opacity: 0, y: 50 },
-//     {
-//       opacity: 1,
-//       y: 0,
-//       stagger: 0.05,
-//       duration: 0.6,
-//       ease: "power3.out",
-//     }
-//   );
-// });
-
-// Select the h1 element
+// Animation
 const text = document.querySelector(".animate-text");
 const chars = text.textContent.split("");
 text.textContent = "";
