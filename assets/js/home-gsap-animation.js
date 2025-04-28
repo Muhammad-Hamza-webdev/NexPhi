76890176475aebfc58c30a23f3536bc6c7ac5ece
Muhammad Hamza <muhammadhamza.webdev@gmail.com>
2025-04-29 01:47:43 +0530

    update animation

--- assets/js/home-gsap-animation.js
+++ assets/js/home-gsap-animation.js
@@ -21,3 +21,52 @@ gsap.from(spans, {
   ease: "power2.out",
   duration: 0.6,
 });
+
+function prepareText(element) {
+  if (element.hasPrepared) return;
+
+  element.hasPrepared = true;
+
+  const chars = element.textContent.split("");
+  element.textContent = "";
+  chars.forEach((char) => {
+    if (char === " ") {
+      element.appendChild(document.createTextNode(" "));
+    } else {
+      const span = document.createElement("span");
+      span.textContent = char;
+      span.style.display = "inline-block";
+      element.appendChild(span);
+    }
+  });
+}
+function animateText(element) {
+  const spans = element.querySelectorAll("span");
+
+  gsap.from(spans, {
+    y: 100,
+    opacity: 0,
+    stagger: 0.03,
+    ease: "power2.out",
+    duration: 0.6,
+  });
+}
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && entry.intersectionRatio >= 0.7) {
+        animateText(entry.target);
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.7,
+  }
+);
+
+document.querySelectorAll(".animation-text-two").forEach((element) => {
+  prepareText(element);
+  observer.observe(element);
+});
